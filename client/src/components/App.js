d8ebbc309c07bdc7d1225f98aa29d3842d170f80
Phillup Zukowksi <phillup.zukowski@gmail.com>
2022-06-18 17:36:53 -0400

    almost complete

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Route, Switch } from "react-router-dom"
+import { Switch, Route } from "react-router-dom"
 import styled from 'styled-components'
 import Home from './Home/Home.js'
 
